7a1e56ccc4ad9c6caa12046e1077f2db695174bb
Dean Quinney <slpixe@gmail.com>
2024-06-01 13:06:27 +0100

    get lines 0 and 1 and hamcrest assert each individually

--- src/test/java/org/example/ThingTest.java
+++ src/test/java/org/example/ThingTest.java
@@ -3,27 +3,32 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 class ThingTest {
     @Test
     public void givenFileNameAsAbsolutePath_whenUsingClasspath_thenFileData() throws IOException {
-        String expectedData = "Hello, world!";
+//        String expectedData = "Hello, world!";
+        List<String> expectedList = List.of("Hello, world!", "My Name is bob");
 
 //        Class<Thing> clazz = Thing.class;
 //        InputStream inputStream = clazz.getResourceAsStream("/fileTest.txt");
 //        String data = Thing.readFromInputStream(inputStream);
 
         Path file = Paths.get("src/main/resources/fileTest.txt");
-        String data = Files.readAllLines(file, StandardCharsets.UTF_8).get(0);
+        String dataZero = Files.readAllLines(file, StandardCharsets.UTF_8).get(0);
+        String dataOne = Files.readAllLines(file, StandardCharsets.UTF_8).get(1);
 
-        assertThat(data, containsString(expectedData));
+//        assertThat(data, containsString(expectedData));
+        assertThat(dataZero, equalTo(expectedList.get(0)));
+        assertThat(dataOne, equalTo(expectedList.get(1)));
     }
 }
